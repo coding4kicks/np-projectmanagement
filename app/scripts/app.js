cd53f9f0cd6d2ecf685a8dd6759ce6f88dd0d4ea
Poof Young <poof@coding4kicks.com>
2013-10-23 20:48:05 -0700

    Create routes for charts and data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,18 @@ angular.module('timWhitneyApp', [])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/undefined', {
+        templateUrl: 'views/undefined.html',
+        controller: 'UndefinedCtrl'
+      })
+      .when('/charts', {
+        templateUrl: 'views/charts.html',
+        controller: 'ChartsCtrl'
+      })
+      .when('/data', {
+        templateUrl: 'views/data.html',
+        controller: 'DataCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/charts.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('timWhitneyApp')
+  .controller('ChartsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

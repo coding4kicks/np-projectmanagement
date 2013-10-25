bf175628ad2753becf5a3c19f5f2e49dcb5c75a9
Poof Young <poof@coding4kicks.com>
2013-10-24 18:20:00 -0700

    Add chart samples.

--- app/scripts/controllers/charts.js
+++ app/scripts/controllers/charts.js
@@ -2,9 +2,5 @@
 
 angular.module('timWhitneyApp')
   .controller('ChartsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });

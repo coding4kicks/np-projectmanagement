8e2b2976d3fec86fda4b531235dc49204b50db37
Poof Young <poof@coding4kicks.com>
2013-10-24 20:40:36 -0700

    Test chart creation.

--- test/spec/controllers/charts.js
+++ test/spec/controllers/charts.js
@@ -6,21 +6,37 @@ describe('Controller: ChartsCtrl', function () {
   beforeEach(module('timWhitneyApp'));
 
   var ChartsCtrl,
-      scope;
+      scope,
+      window;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, $window) {
     scope = $rootScope.$new();
-    $window.Morris = {};
-    $window.Morris.Bar = jasmine.createSpy('Bar');
-    $window.Morris.Donut = jasmine.createSpy('Donut');
-    $window.Morris.Line = jasmine.createSpy('Line');
-    $window.Morris.Area = jasmine.createSpy('Area');
+    window = $window;
+    window.Morris = {};
+    window.Morris.Bar = jasmine.createSpy('Bar');
+    window.Morris.Donut = jasmine.createSpy('Donut');
+    window.Morris.Line = jasmine.createSpy('Line');
+    window.Morris.Area = jasmine.createSpy('Area');
     ChartsCtrl = $controller('ChartsCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('should create a bar chart', function () {
+    expect(window.Morris.Bar).toHaveBeenCalled();
   });
+
+  it('should create a donut chart', function () {
+    expect(window.Morris.Donut).toHaveBeenCalled();
+  });
+
+  it('should create a line chart', function () {
+    expect(window.Morris.Line).toHaveBeenCalled();
+  });
+
+  it('should create an area chart', function () {
+    expect(window.Morris.Area).toHaveBeenCalled();
+  });
+
 });
